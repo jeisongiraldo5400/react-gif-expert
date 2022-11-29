7de6baf2351c84bd7b9f2b56508d7756866bfa98
jeison.giraldo <jeison.giraldo@ecomtrading.com>
2022-11-29 14:15:36 -0500

    test

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,16 +1,47 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
 
 describe("Pruebas en <GifGrid />", () => {
   const category = "One Punch";
 
   test("debe de mostrar el loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
     //screen.debug();
 
-    expect(screen.getByText("Cargando.."));
+    expect(screen.getByText("Cargando..."));
     expect(screen.getByText(category));
   });
 
-  test("debe mostrar items cuando se cargan las imagenes useFetchGifs", () => {});
+  test("debe mostrar items cuando se cargan las imagenes useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg",
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "https://localhost/saitama.jpg",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+    //screen.debug();
+
+    expect(screen.getAllByRole("img").length).toBe(2);
+  });
 });
